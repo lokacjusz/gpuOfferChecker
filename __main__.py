2e8e1df3881445b55d300c027555e423d1c572dd
lokacjusz <49211830+lokacjusz@users.noreply.github.com>
2021-03-31 16:04:13 +0200

    bug fix - try/catch added

--- __main__.py
+++ __main__.py
@@ -19,27 +19,33 @@ url = "https://www.pepper.pl/grupa/karty-graficzne"
 ##list all tags
 #print([tag.name for tag in soup.find_all()])
 ##
-
+i=0
 while(True):
-    html_doc = requests.get(url)
-    soup = BeautifulSoup(html_doc.text,'html.parser')
-    ids = [tag['id'] for tag in soup.select('article[id]')]
-    img = soup.find_all('img')
-    GPU_name = (img[1].get('alt'))
-    with open('X:\\python\\pepper_gpu_checker\\id.txt', 'a+') as file:
-        file.seek(0) ##set cursor on zero char - at the very first char
-        data = file.read().replace('\n', '')
-        #print("data: {0}".format(data))
-        #file.write(ids[0])
-        if str(ids[0]).strip() != str(data).strip():
-            #print(ids[0])
-            new_offer = soup.article.a.get("href")
-            print(soup.article.a.get("href"))
-            driver = webdriver.Firefox(executable_path="X:\\python\\pepper_gpu_checker\\geckodriver.exe")
-            #driver.get(new_offer)
-            driver.get(url)
-            file.truncate(0)
-            file.write(ids[0])
-            file.close()
-            popup_tray.popup_notification(img[1].get('alt'))
-    time.sleep(5*60)
+    print("check no. 1")
+    try:
+        html_doc = requests.get(url)
+        soup = BeautifulSoup(html_doc.text,'html.parser')
+        ids = [tag['id'] for tag in soup.select('article[id]')]
+        img = soup.find_all('img')
+        GPU_name = (img[1].get('alt'))
+        with open('X:\\python\\pepper_gpu_checker\\id.txt', 'a+') as file:
+            file.seek(0) ##set cursor on zero char - at the very first char
+            data = file.read().replace('\n', '')
+            #print("data: {0}".format(data))
+            #file.write(ids[0])
+            if str(ids[0]).strip() != str(data).strip():
+                #print(ids[0])
+                new_offer = soup.article.a.get("href")
+                print(soup.article.a.get("href"))
+                driver = webdriver.Firefox(executable_path="X:\\python\\pepper_gpu_checker\\geckodriver.exe")
+                #driver.get(new_offer)
+                driver.get(url)
+                file.truncate(0)
+                file.write(ids[0])
+                file.close()
+                popup_tray.popup_notification(img[1].get('alt'))
+            else:
+                print("no updates")
+    except:
+        print("failure. retry after 5 minutes")
+    time.sleep(360)
